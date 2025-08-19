b07c202e26c0ef1aa6d0dcc70c28d882927353db
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-19 18:56:30 +0100

    Drug dispensary system

---
+++ src/main/java/com/pharmacy/data/repository/Prescriptions.java
@@ -0,0 +1,9 @@
+package com.pharmacy.data.repository;
+
+import com.pharmacy.data.models.Prescription;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prescriptions {
+    static List<Prescription> prescriptions = new ArrayList<>();
+}
