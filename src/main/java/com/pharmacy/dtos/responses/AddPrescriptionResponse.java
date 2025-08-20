a6276a882d83acf760d5d377440ce5dfca5053cb
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-20 10:24:01 +0100

    Response for doctors upon making Prescription

--- src/main/java/com/pharmacy/dtos/responses/AddPrescriptionResponse.java
+++ src/main/java/com/pharmacy/dtos/responses/AddPrescriptionResponse.java
@@ -1,4 +1,43 @@
 package com.pharmacy.dtos.responses;
 
+import com.pharmacy.data.models.Drug;
+
 public class AddPrescriptionResponse {
+    private long patientID;
+    private Drug drug;
+    private String dosage;
+    private int quantity;
+
+
+    public long getPatientID() {
+        return this.patientID;
+    }
+
+    public void setPatientID(long patientID) {
+        this.patientID = patientID;
+    }
+
+    public Drug getDrug() {
+        return this.drug;
+    }
+
+    public void setDrug(Drug drug) {
+        this.drug = drug;
+    }
+
+    public String getDosage() {
+        return this.dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
