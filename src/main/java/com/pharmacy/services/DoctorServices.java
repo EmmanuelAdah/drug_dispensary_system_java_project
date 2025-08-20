dadff010b7c4feb5ab3c5047afd2fe343784977a
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-20 10:25:03 +0100

    Doctor's services for making prescriptions and diagnosis

--- src/main/java/com/pharmacy/services/DoctorServices.java
+++ src/main/java/com/pharmacy/services/DoctorServices.java
@@ -1,4 +1,18 @@
 package com.pharmacy.services;
 
+import com.pharmacy.data.models.Prescription;
+import com.pharmacy.data.repository.Prescriptions;
+import com.pharmacy.dtos.request.AddPrescriptionRequest;
+import com.pharmacy.dtos.responses.AddPrescriptionResponse;
+import static com.pharmacy.utils.Mapper.map;
+
 public class DoctorServices {
+    private final Prescriptions prescriptions = new Prescriptions();
+
+    public AddPrescriptionResponse addPrescription(AddPrescriptionRequest addPrescriptionRequest) {
+        Prescription prescription = map(addPrescriptionRequest);
+        prescriptions.savePrescription(prescription);
+        AddPrescriptionResponse addPrescriptionResponse = map(prescription);
+        return addPrescriptionResponse;
+    }
 }
