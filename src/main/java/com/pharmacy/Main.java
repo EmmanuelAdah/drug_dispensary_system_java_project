01bf4da55ff0997479c49d99774fed6afd26b085
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-21 13:07:59 +0100

    User interface for the drug dispensary system

--- src/main/java/com/pharmacy/Main.java
+++ src/main/java/com/pharmacy/Main.java
@@ -1,7 +1,52 @@
 package com.pharmacy;
 
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
 public class Main {
     public static void main(String[] args) {
 
+        String choice = input(mainMenu());
+
+    }
+
+    public static String mainMenu() {
+        return """
+                welcome
+                press:-
+                1 -> Admin
+                2 -> Doctor
+                3 -> Pharmacist
+                4 -> Exit
+                """;
+    }
+
+    public static String doctorMenu(){
+        return """
+                press:-
+                1 -> Make prescription
+                2 -> View prescriptions
+                0 -> Back
+                """;
+    }
+
+   public static String pharmacistMenu(){
+        return """
+                press:-
+                1 -> Check prescription
+                2 -> Dispense drugs
+                0 -> Back
+                """;
+   }
+
+    public static void print(String message) {
+        System.out.println(message);
+    }
+
+    public static String input(String message) {
+        print(message);
+        Scanner scanner = new Scanner(in);
+        return scanner.nextLine();
     }
 }
