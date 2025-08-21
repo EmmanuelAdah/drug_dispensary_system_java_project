7c071e1eacb61f3bdc5ab8061b7b2dd1f4aee052
therabbi.tim@gmail.com <you@example.com>
2025-08-21 11:36:52 +0100

    edited attribute for patient

--- src/main/java/com/pharmacy/data/models/Patient.java
+++ src/main/java/com/pharmacy/data/models/Patient.java
@@ -4,9 +4,7 @@ public class Patient {
     private long patientID;
     private String firstName;
     private String lastName;
-    private int age;
     private Gender gender;
-    private String email;
     private String phone;
 
     public long getPatientID() {
@@ -25,36 +23,12 @@ public class Patient {
         this.firstName = firstName;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public String getLastName() {
         return lastName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getPhone() {
