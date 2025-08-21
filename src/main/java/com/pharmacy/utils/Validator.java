38580d923e182c286bae157127dd63a18b35cb2b
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-21 17:42:46 +0100

    Validation of prescription entities

--- src/main/java/com/pharmacy/utils/Validator.java
+++ src/main/java/com/pharmacy/utils/Validator.java
@@ -1,24 +1,12 @@
 package com.pharmacy.utils;
 
 import com.pharmacy.data.models.Drug;
-import com.pharmacy.data.models.Prescription;
-import com.pharmacy.dtos.request.AddPrescriptionRequest;
 import com.pharmacy.exceptions.InvalidDrugQuantityException;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Validator {
 
-    public static void validate(AddPrescriptionRequest addPrescriptionRequest) {
-        if (addPrescriptionRequest.getQuantity() <= 0)
+    public static void validate(Drug drug) {
+        if (drug.getQuantity() <= 0)
             throw new InvalidDrugQuantityException("Drug quantity must be greater than 0");
-    }
-
-    public static List<Drug> validateDrugs(Prescription prescription) {
-        List<Drug> drugs = new ArrayList<>();
-        for (Drug drug : prescription.getDrugs()) {
-            
-        }
     }
 }
