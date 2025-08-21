b7ac3d896775122eec8fa4e1c97e8dd60d47a8b4
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-21 13:08:47 +0100

    Mapper for doctors services

--- src/main/java/com/pharmacy/utils/Mapper.java
+++ src/main/java/com/pharmacy/utils/Mapper.java
@@ -1,14 +1,18 @@
 package com.pharmacy.utils;
 
 import com.pharmacy.data.models.Prescription;
+import com.pharmacy.data.models.Status;
 import com.pharmacy.dtos.request.AddPrescriptionRequest;
+import com.pharmacy.dtos.request.CancelPrescriptionRequest;
 import com.pharmacy.dtos.responses.AddPrescriptionResponse;
+import com.pharmacy.dtos.responses.CancelPrescriptionResponse;
 
 public class Mapper {
 
     public static Prescription map(AddPrescriptionRequest addPrescriptionRequest) {
         Prescription prescription = new Prescription();
         prescription.setPatientID(addPrescriptionRequest.getPatientID());
+        prescription.setPrescriptionID(prescription.getPrescriptionID());
         prescription.setDiagnosis(addPrescriptionRequest.getDiagnosis());
         prescription.setDrug(addPrescriptionRequest.getDrug());
         prescription.setDosage(addPrescriptionRequest.getDosage());
@@ -23,5 +27,21 @@ public class Mapper {
         addPrescriptionResponse.setDosage(prescription.getDosage());
         addPrescriptionResponse.setQuantity(prescription.getQuantity());
         return addPrescriptionResponse;
+    }
+
+    public static Prescription mapper(CancelPrescriptionRequest cancelPrescriptionRequest){
+        Prescription prescription = new Prescription();
+        prescription.setPatientID(cancelPrescriptionRequest.getPatientID());
+        prescription.setPrescriptionID(cancelPrescriptionRequest.getPrescriptionID());
+        prescription.setStatus(Status.CANCELLED);
+        return prescription;
+    }
+
+    public static CancelPrescriptionResponse mapper(Prescription prescription){
+        CancelPrescriptionResponse response = new CancelPrescriptionResponse();
+        response.setPatientId(prescription.getPatientID());
+        response.setPrescriptionId(prescription.getPrescriptionID());
+        response.setStatus(Status.CANCELLED);
+        return response;
     }
 }
