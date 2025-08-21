f67bdc29a3d26327e4bfb707d975b5b61ddf6552
therabbi.tim@gmail.com <you@example.com>
2025-08-21 18:35:39 +0100

    created the code generator for each prescription

--- src/main/java/com/pharmacy/data/models/Prescription.java
+++ src/main/java/com/pharmacy/data/models/Prescription.java
@@ -1,15 +1,69 @@
 package com.pharmacy.data.models;
 
-import lombok.Data;
-
-import java.util.List;
-
-@Data
 public class Prescription {
-    private int patientID;
-    private String diagnosis;
-    private List<Drug> drugs;
-    private Status status;
-    private String prescriptionCode;
+    private long patientID;
+    private Drug drug;
+    private Doctor doctor;
+    private int dosage;
+    private int quantity;
+    private String status;
+    private String code;
 
+    public long getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(long patientID) {
+        this.patientID = patientID;
+    }
+
+    public Drug getDrug() {
+        return drug;
+    }
+
+    public void setDrug(Drug drug) {
+        this.drug = drug;
+    }
+
+    public int getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(int dosage) {
+        this.dosage = dosage;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public void add(Prescription prescription) {
+        prescription.setPatientID(patientID);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String s) {
+    }
 }
