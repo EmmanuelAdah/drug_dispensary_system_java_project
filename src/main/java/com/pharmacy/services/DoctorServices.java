16a26cd8b7f9cf0e47f10757efddcb999ce6dbe9
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-21 13:07:01 +0100

    Doctors services in the pharmacy

--- src/main/java/com/pharmacy/services/DoctorServices.java
+++ src/main/java/com/pharmacy/services/DoctorServices.java
@@ -3,8 +3,12 @@ package com.pharmacy.services;
 import com.pharmacy.data.models.Prescription;
 import com.pharmacy.data.repository.Prescriptions;
 import com.pharmacy.dtos.request.AddPrescriptionRequest;
+import com.pharmacy.dtos.request.CancelPrescriptionRequest;
 import com.pharmacy.dtos.responses.AddPrescriptionResponse;
+import com.pharmacy.dtos.responses.CancelPrescriptionResponse;
+
 import static com.pharmacy.utils.Mapper.map;
+import static com.pharmacy.utils.Mapper.mapper;
 import static com.pharmacy.utils.Validator.validate;
 
 public class DoctorServices {
@@ -18,11 +22,9 @@ public class DoctorServices {
         return addPrescriptionResponse;
     }
 
-    public void deletePrescription(AddPrescriptionRequest addPrescriptionRequest){
-        validate(addPrescriptionRequest);
-        Prescription prescription = map(addPrescriptionRequest);
-        prescriptions.deletePrescription(prescription);
-
+    public CancelPrescriptionResponse cancelPrescription(CancelPrescriptionRequest cancelPrescriptionRequest){
+        Prescription prescription = mapper(cancelPrescriptionRequest);
+        prescriptions.cancelPrescription(prescription);
+        return mapper(prescription);
     }
-
 }
