3f1f436e0a8916f81eab4a1c2e2a78698514997f
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-22 18:47:43 +0100

    User interface for the drug dispensary app

--- src/main/java/com/pharmacy/Main.java
+++ src/main/java/com/pharmacy/Main.java
@@ -1,31 +1,46 @@
 package com.pharmacy;
 
+import com.pharmacy.dtos.request.AddPrescriptionRequest;
+
 import java.util.Scanner;
 
 import static java.lang.System.in;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String... args) {
 
-        String choice = input(mainMenu());
+        String choice = input(mainMenu() + "Enter option: ");
         switch (choice){
             case "1":
-                print(adminMenu());
                 while (true){
                     String username = input("Enter username");
                     String password = input("Enter password");
 
                     String response = input("Would you like to add another user? (y/n)");
-                    if (response.equalsIgnoreCase("no") || response.equalsIgnoreCase("n")) break;
+                    switch (response){
+                        case "yes", "y": continue;
+                        default: main();
+                    }
                 }
             case "2":
                 print(doctorMenu());
+                String doctorsChoice = input("Enter option: ");
+                switch (doctorsChoice){
+                    case "1": {
+
+
+                    }
+                    case "2":{
+
+                    }
+                }
                 break;
             case "3":
                 print(pharmacistMenu());
                 break;
-        }
 
+            case "4": print("Good bye..."); System.exit(12);
+        }
 
     }
 
@@ -37,14 +52,6 @@ public class Main {
                 2 -> Doctor
                 3 -> Pharmacist
                 4 -> Exit
-                """;
-    }
-
-    public static String adminMenu(){
-        return """
-                press:-
-                1 -> Add user
-                0 -> Back
                 """;
     }
 
@@ -64,6 +71,15 @@ public class Main {
                 2 -> Dispense drugs
                 0 -> Back
                 """;
+   }
+
+   public static AddPrescriptionRequest makePrescription(){
+        AddPrescriptionRequest request = new AddPrescriptionRequest();
+        while (true){
+            request.setPatientID(Integer.parseInt(input("Enter patient ID: ")));
+
+        }
+
    }
 
     public static void print(String message) {
