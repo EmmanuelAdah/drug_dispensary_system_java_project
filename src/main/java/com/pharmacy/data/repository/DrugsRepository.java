3ecdcc251be2333f2cc17c8cbf3b38dfe5fb70f9
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-22 18:49:00 +0100

    Drugs repository for all drugs in the pharmacy

--- src/main/java/com/pharmacy/data/repository/DrugsRepository.java
+++ src/main/java/com/pharmacy/data/repository/DrugsRepository.java
@@ -4,14 +4,14 @@ import com.pharmacy.data.models.Drug;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Drugs {
-    static List<ArrayList<Drug>> drugs = new ArrayList<>();
+public class DrugsRepository {
+    static List<Drug> drugs = new ArrayList<>();
 
-    public void saveDrug(ArrayList<Drug> drugs_){
+    public void saveDrug(Drug drugs_){
         drugs.add(drugs_);
     }
 
-    public List<ArrayList<Drug>> getDrugs(){
+    public List<Drug> getDrugs(){
         return drugs;
     }
 }
