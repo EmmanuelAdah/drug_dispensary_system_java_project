61a57bab73e7d05217325543a790f471a8906df0
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-22 18:46:43 +0100

    Doctor's services to patients

--- src/main/java/com/pharmacy/services/DoctorServices.java
+++ src/main/java/com/pharmacy/services/DoctorServices.java
@@ -1,5 +1,30 @@
 package com.pharmacy.services;
 
-public class DoctorServices {
+import com.pharmacy.data.models.Prescription;
+import com.pharmacy.data.repository.PrescriptionsRepository;
+import com.pharmacy.dtos.request.AddPrescriptionRequest;
+import com.pharmacy.dtos.request.CancelPrescriptionRequest;
+import com.pharmacy.dtos.responses.AddPrescriptionResponse;
+import com.pharmacy.dtos.responses.CancelPrescriptionResponse;
+import static com.pharmacy.utils.Mapper.map;
+import static com.pharmacy.utils.Mapper.mapper;
 
+public class DoctorServices {
+    private final PrescriptionsRepository prescriptionsRepository = new PrescriptionsRepository();
+
+    public AddPrescriptionResponse addPrescription(AddPrescriptionRequest addPrescriptionRequest) {
+        Prescription prescription = map(addPrescriptionRequest);
+        prescriptionsRepository.savePrescription(prescription);
+        return map(prescription);
+    }
+
+    public void viewPrescriptions(){
+        prescriptionsRepository.viewPrescriptions();
+    }
+
+    public CancelPrescriptionResponse cancelPrescription(CancelPrescriptionRequest cancelPrescriptionRequest){
+        Prescription prescription = mapper(cancelPrescriptionRequest);
+        prescriptionsRepository.cancelPrescription(prescription);
+        return mapper(prescription);
+    }
 }
