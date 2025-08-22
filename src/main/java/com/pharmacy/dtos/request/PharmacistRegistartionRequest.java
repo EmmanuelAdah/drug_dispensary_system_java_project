c2dbe52b2670fcb462ec5b2e9c2d2ea61fbd4aad
hassan <salakohassanmobolaji@gmail.com>
2025-08-22 13:26:07 +0100

    Doctor registration for prescription of drugs to patient

---
+++ src/main/java/com/pharmacy/dtos/request/PharmacistRegistartionRequest.java
@@ -0,0 +1,45 @@
+package com.pharmacy.dtos.request;
+
+public class PharmacistRegistartionRequest {
+        private String firstname;
+        private String lastname;
+        private String password;
+        private String phoneNumber;
+        private String email;
+
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        } public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+        public String getFirstname() {
+            return firstname;
+        }
+
+        public void setFirstname(String firstname) {
+            this.firstname = firstname;
+        }
+        public String getLastname() {
+            return lastname;
+        }
+        public void setLastname(String lastname) {
+            this.lastname = lastname;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+        public void setPassword(String password) {
+            this.password = password;
+        }
+        public String getEmail() {
+            return email;
+        }
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+
+
+
