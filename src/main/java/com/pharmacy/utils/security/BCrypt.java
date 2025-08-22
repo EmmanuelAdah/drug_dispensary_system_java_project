1803cddd4fcaeaa9d0e5b2ec5724f9dfa5d3ad45
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-22 12:59:57 +0100

    For hashing user's password

--- src/main/java/com/pharmacy/utils/security/BCrypt.java
+++ src/main/java/com/pharmacy/utils/security/BCrypt.java
@@ -3,7 +3,7 @@ package com.pharmacy.utils.security;
 public class BCrypt {
 
         public static String hashPassword(String password) {
-            return com.pharmacy.utils.security.BCrypt.hashpw(password, com.pharmacy.utils.security.BCrypt.gensalt(12));
+            return BCrypt.hashpw(password, com.pharmacy.utils.security.BCrypt.gensalt(12));
         }
 
 }
