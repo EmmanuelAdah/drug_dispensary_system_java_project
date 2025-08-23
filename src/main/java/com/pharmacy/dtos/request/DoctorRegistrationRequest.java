77a24cd0401bf45d0b2f7cbb2099ccab9680c207
hassan <salakohassanmobolaji@gmail.com>
2025-08-23 21:42:57 +0100

    update of class DoctorRegistrationRequest

---
+++ src/main/java/com/pharmacy/dtos/request/DoctorRegistrationRequest.java
@@ -0,0 +1,52 @@
+package com.pharmacy.dtos.request;
+
+import com.pharmacy.data.models.Specialty;
+
+public class DoctorRegistrationRequest {
+
+        private String firstname;
+        private String lastname;
+        private Specialty specialty;
+        private String password;
+        private String email;
+        private String phoneNumber;
+
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+        public String getEmail() {
+            return email;
+        }
+        public void setEmail(String email) {
+            this.email = email;
+        }
+        public String getPassword() {
+            return password;
+        }
+        public void setPassword(String password) {
+            this.password = password;
+        }
+        public Specialty getSpecialty() {
+            return specialty;
+        }
+        public void setSpecialty(Specialty specialty) {
+            this.specialty = specialty;
+        }
+        public String getLastname() {
+            return lastname;
+        }
+        public void setLastname(String lastname) {
+            this.lastname = lastname;
+        }
+        public String getFirstname() {
+            return firstname;
+        }
+        public void setFirstname(String firstname) {
+            this.firstname = firstname; }
+    }
+
+
