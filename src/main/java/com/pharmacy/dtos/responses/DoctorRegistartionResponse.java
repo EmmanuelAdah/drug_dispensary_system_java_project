17da75c94b23dddd2e9eaa305f2b35e2b1c1edb2
hassan <salakohassanmobolaji@gmail.com>
2025-08-23 21:41:44 +0100

    update of class DoctorRegistaionResponse

--- src/main/java/com/pharmacy/dtos/responses/DoctorRegistartionResponse.java
+++ src/main/java/com/pharmacy/dtos/responses/DoctorRegistartionResponse.java
@@ -10,38 +10,44 @@ public class DoctorRegistartionResponse {
         private Specialty specialty;
 
         public Specialty getSpecialty() {
+
             return specialty;
         }
 
         public void setSpecialty(Specialty specialty) {
+
             this.specialty = specialty;
         }
 
 
         public String getLastname() {
+
             return lastname;
         }
 
         public void setLastname(String lastname) {
+
             this.lastname = lastname;
         }
 
 
         public String getFirstname() {
+
             return firstname;
         }
 
         public void setFirstname(String firstname) {
+
             this.firstname = firstname;
         }
 
-
-
         public long getDoctorID() {
+
             return doctorID;
         }
 
         public void setDoctorID(long doctorID) {
+
             this.doctorID = doctorID;
         }
 
@@ -50,6 +56,7 @@ public class DoctorRegistartionResponse {
             this.message = input;
         }
         public String getMessage() {
+
             return this.message;
         }
 
