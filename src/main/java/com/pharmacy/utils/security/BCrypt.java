c0acc24493be271a63b835f797797f36a7770611
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-24 21:48:56 +0100

    For encryption of patient's data

--- src/main/java/com/pharmacy/utils/security/BCrypt.java
+++
@@ -1,9 +0,0 @@
-//package com.pharmacy.utils.security;
-//
-//public class BCrypt {
-//
-//        public static String hashPassword(String password) {
-//            return BCrypt.hashpw(password, com.pharmacy.utils.security.BCrypt.gensalt(12));
-//        }
-//
-//}
