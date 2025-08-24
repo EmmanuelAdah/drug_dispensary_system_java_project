cf117aed53724d886b8cd8847bb2003a356f33a3
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-24 21:49:58 +0100

    for doctors to add drug request for dispensary

--- src/main/java/com/pharmacy/dtos/request/AddPrescriptionRequest.java
+++ src/main/java/com/pharmacy/dtos/request/AddPrescriptionRequest.java
@@ -1,52 +1,14 @@
 package com.pharmacy.dtos.request;
 
 import com.pharmacy.data.models.Drug;
+import lombok.Data;
 import java.util.List;
 
+@Data
 public class AddPrescriptionRequest {
-    private long patientID;
+    private int patientID;
     private String diagnosis;
     private List<Drug> drugs;
     private String dosage;
     private int quantity;
-
-    public long getPatientID() {
-        return this.patientID;
-    }
-
-    public void setPatientID(long patientID) {
-        this.patientID = patientID;
-    }
-
-    public String getDiagnosis() {
-        return this.diagnosis;
-    }
-
-    public void setDiagnosis(String diagnosis) {
-        this.diagnosis = diagnosis;
-    }
-
-    public List<Drug> getDrugs() {
-        return this.drugs;
-    }
-
-    public void setDrug(List<Drug> drug) {
-        this.drugs = drug;
-    }
-
-    public String getDosage() {
-        return this.dosage;
-    }
-
-    public void setDosage(String dosage) {
-        this.dosage = dosage;
-    }
-
-    public int getQuantity() {
-        return this.quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
 }
