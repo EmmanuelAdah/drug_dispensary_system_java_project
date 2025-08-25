ec2541ea0d7da47697070b556233b7c70e00feef
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-25 11:19:21 +0100

    patient entity with th respective fields

--- src/main/java/com/pharmacy/data/models/Patient.java
+++ src/main/java/com/pharmacy/data/models/Patient.java
@@ -8,6 +8,32 @@ public class Patient {
     private Gender gender;
     private String email;
     private String phone;
+    private String username;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 
     public int getPatientID() {
         return patientID;
