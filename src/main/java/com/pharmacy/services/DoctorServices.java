0b8b4bf18b28024fc79cfa7c78ac95a1e3d92c72
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-27 21:44:21 +0100

    Doctor's services for handling prescriptions

--- src/main/java/com/pharmacy/services/DoctorServices.java
+++ src/main/java/com/pharmacy/services/DoctorServices.java
@@ -6,21 +6,21 @@ import com.pharmacy.dtos.request.AddPrescriptionRequest;
 import com.pharmacy.dtos.request.CancelPrescriptionRequest;
 import com.pharmacy.dtos.responses.AddPrescriptionResponse;
 import com.pharmacy.dtos.responses.CancelPrescriptionResponse;
-
+import java.util.List;
 import static com.pharmacy.utils.Mapper.map;
 import static com.pharmacy.utils.Mapper.mapper;
 
 public class DoctorServices {
     private static final PrescriptionsRepository prescriptionsRepository = new PrescriptionsRepository();
 
-    public static AddPrescriptionResponse addPrescription(AddPrescriptionRequest addPrescriptionRequest) {
+    public AddPrescriptionResponse addPrescription(AddPrescriptionRequest addPrescriptionRequest) {
         Prescription prescription = map(addPrescriptionRequest);
         prescriptionsRepository.savePrescription(prescription);
         return map(prescription);
     }
 
-    public void viewPrescriptions(){
-        prescriptionsRepository.viewPrescriptions();
+    public List<Prescription> viewPrescriptions(){
+        return prescriptionsRepository.viewPrescriptions();
     }
 
     public CancelPrescriptionResponse cancelPrescription(CancelPrescriptionRequest cancelPrescriptionRequest){
