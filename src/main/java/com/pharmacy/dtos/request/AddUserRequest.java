dbbbfdaddcebe65954209ea6c8fa42989438f073
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-27 21:41:02 +0100

    User request entity for the required user's field

---
+++ src/main/java/com/pharmacy/dtos/request/AddUserRequest.java
@@ -0,0 +1,15 @@
+package com.pharmacy.dtos.request;
+
+
+import com.pharmacy.data.models.Role;
+import lombok.Data;
+
+@Data
+public class AddUserRequest {
+    private long userID;
+    private String firstName;
+    private String lastName;
+    private String role;
+    private String username;
+    private String password;
+}
